f7b9db988eea510d4534f602dddc1a7a8978cceb
Teniola Ogunlaja <131896786+callmeteni@users.noreply.github.com>
2024-11-15 23:26:46 -0600

    Updates

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export mode
-  basePath: '/<repository-name>', // Replace with your GitHub repository name
-  trailingSlash: true, // Ensures proper routing for static export
-};
-
-export default nextConfig;
